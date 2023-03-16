c106d732ee83d680d3eaa1360490f1302a1911b8
edoka <peter.edoka.242464@unn.edu.ng>
2023-03-16 16:05:05 +0100

    print sizes of datatypes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point function
+ * prints the siaze of various types on the host computer
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char %ld bytes(s)\n", sizeof(char));
+	printf("Size of a int %ld bytes(s)\n", sizeof(int));
+	printf("Size of a long int %ld bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int %ld bytes(s)\n", sizeof(long long int));
+	printf("Size of a float %ld bytes(s)\n", sizeof(float));
+	return (0);
+}
