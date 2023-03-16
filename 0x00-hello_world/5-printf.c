53ca4d7ccc43d371f0bd61d2401bbac00c2046b7
edoka <peter.edoka.242464@unn.edu.ng>
2023-03-16 15:29:40 +0100

    prints string to stdout using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point function
+ * It prints out a string to the stdout using printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
