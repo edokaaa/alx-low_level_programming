0881ed1fb23b1e3bee6c77cb4bfa254d4fcf7cc1
edoka <peter.edoka.242464@unn.edu.ng>
2023-03-16 15:23:39 +0100

    prints string using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * This function prints a string to the standard output
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
