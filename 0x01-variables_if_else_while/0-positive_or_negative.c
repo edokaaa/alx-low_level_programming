35cdd47592ebcb427f2479b4381ab24b9d170374
edoka <peter.edoka.242464@unn.edu.ng>
2023-03-17 14:22:25 +0100

    if else

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Checks if a number is positive or negative
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive", n);
+	} else if (n == 0)
+	{
+		printf("%d is zero", n);
+	} else
+	{
+		printf("%d is negative", n);
+	}
+	return (0);
+}
