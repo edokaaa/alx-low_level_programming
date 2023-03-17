2b0169b47f918b5dfc50cce41ade04266c153963
edoka <peter.edoka.242464@unn.edu.ng>
2023-03-17 16:17:22 +0100

    print 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers 0 - 9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+	printf("\n");
+}
