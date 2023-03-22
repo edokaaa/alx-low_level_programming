83a59df9227ea3f8dfe65a81da6b0a8a4f7f0d9a
edoka <peter.edoka.242464@unn.edu.ng>
2023-03-22 12:38:17 +0100

    new: n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - function that prints the 9 times table
+ * @n: the input number
+ *
+ * Return: 0
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (n > 15 || n < 0)
+		return;
+
+	j = 0;
+	while (j <= n)
+	{
+		i = 0;
+		while (i <= n)
+		{
+			if (i == n)
+			{
+				printf("%3d\n", i * j);
+			} else if (i == 0)
+			{
+				printf("%d, ", i * j);
+			} else
+			{
+				printf("%3d, ", i * j);
+			}
+			i++;
+		}
+		j++;
+	}
+
+}
