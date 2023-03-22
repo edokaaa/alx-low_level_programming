13aa83d36643b3de9c2d2aa438c7b51adb2a7b1b
edoka <peter.edoka.242464@unn.edu.ng>
2023-03-22 04:09:13 +0100

    islower function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercas character
+ * @c - character to be checked
+ *
+ * Return: 1 on success, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
