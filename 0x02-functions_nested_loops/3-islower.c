b2bfc82261cb081fbce50c8490fe9a21c8995e4b
edoka <peter.edoka.242464@unn.edu.ng>
2023-03-22 04:11:32 +0100

    fix: fixed betty style error

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,17 +2,13 @@
 
 /**
  * _islower - checks for lowercas character
- * @c - character to be checked
+ * @c: character to be checked
  *
  * Return: 1 on success, 0 otherwise
  */
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-	{
 		return (1);
-	} else
-	{
-		return (0);
-	}
+	return (0);
 }
