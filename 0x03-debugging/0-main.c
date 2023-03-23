d6cfb8f8c3de8c163c37a0053a87f3ab79b05df5
edoka <peter.edoka.242464@unn.edu.ng>
2023-03-23 09:22:19 +0100

    new: test positive_or_negative function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - tests that the function positive_or_negative()
+ * gives the correct output when given a case of 0.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	positive_or_negative(0);
+	return (0);
+}
