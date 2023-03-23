f7cd390c5d38d85f9a062c33f3d6d1b0d303efdd
edoka <peter.edoka.242464@unn.edu.ng>
2023-03-23 18:57:51 +0100

    print lines task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line in the terminal.
+ * @n: specifies the length of the line wrt `_`
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n != 0)
+	{
+		while (i < n)
+		{
+			_putchar(95);
+			i++;
+		}
+	}
+	_putchar('\n');
+	return;
+}
