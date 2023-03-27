1846027d49e4b2cf6d947231c3d4cfa6b66edd08
edoka <peter.edoka.242464@unn.edu.ng>
2023-03-27 15:08:12 +0100

    fix: betty style error fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,10 @@
  * reset_to_98 - function that takes a pointer to an int
  * as parameter and updates the value it points to to 98.
  * @n: the pointer to the integer
- * 
+ *
  * Return: Void
 */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
