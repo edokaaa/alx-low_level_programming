5dfe8d29dbd8c9749548a58ba9fd07d8829c9793
edoka <peter.edoka.242464@unn.edu.ng>
2023-03-27 14:33:40 +0100

    exponent function using recursion

---
+++ 0x04-more_functions_nested_loops/_pow.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * pow - calculates the exponent of a number using recursion
+ * a - the base number
+ * b - the exponent
+ * 
+ * Return: a ^ b
+*/
+int _pow(int a, int b)
+{
+	if (b == 0)
+		return (1);
+	if (a == 0)
+		return (0);
+	return (a * _pow(a, b - 1));
+}
