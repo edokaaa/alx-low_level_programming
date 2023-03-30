f8f77dae1c57d1101a7e3246abcbb907402431f4
edoka <peter.edoka.242464@unn.edu.ng>
2023-03-30 18:48:44 +0100

    fix: used 2 loops and 1 if for leet encoding task

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,44 +1,39 @@
+#include "main.h"
+
 /**
  * leet - a function that encodes a string into 1337.
+ * Instructions:
  * Letters a and A should be replaced by 4
  * Letters e and E should be replaced by 3
  * Letters o and O should be replaced by 0
  * Letters t and T should be replaced by 7
  * Letters l and L should be replaced by 1
+ * You can only use one if in your code
+ * You can only use two loops in your code
+ * You are not allowed to use switch
+ * You are not allowed to use any ternary operation
+ *
  * @str: pointer to the string.
  *
  * Return: a pointer to the encoded string.
 */
 char *leet(char *str)
 {
-	int i = 0;
-
+	int i, l;
+	
+	char l1[] = "aAeEoOtTlL";
+	char l2[] = "4433007711";
+	
+	i = 0;
 	while (str[i] != '\0')
 	{
-		while (str[i] == 'a' || str[i] == 'A')
+		for (l = 0; l1[l] != '\0'; l++)
 		{
-			str[i] = '4';
-			break;
-		}
-		while (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = '3';
-			break;
-		}
-		while (str[i] == 'o' || str[i] == 'O')
-		{
-			str[i] = '0';
-			break;
-		}
-		while (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = '7';
-			break;
-		}
-		while (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = '1';
-			break;
+			if (str[i] == l1[l])
+			{
+				str[i] = l2[l];
+				break;
+			}
 		}
 		i++;
 	}
