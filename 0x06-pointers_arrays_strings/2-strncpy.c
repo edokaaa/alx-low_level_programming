5aabf1cf20b4180eb0a91f2a286b4d47b667331f
edoka <peter.edoka.242464@unn.edu.ng>
2023-03-30 12:00:27 +0100

    strncpy() clone

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that copies a string.
+ * this function works exactly like the default strncpy
+ * from the standard library.
+ * It copies at most n bytes of src to dest
+ * @dest: the destination string
+ * @src: the source string
+ * @n: number of bytes to copy
+ * 
+ * Return: Pointer to the dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	/* copy at most n bytes of src to dest */
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	/* pad dest with null bytes if src < n*/
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
+}
