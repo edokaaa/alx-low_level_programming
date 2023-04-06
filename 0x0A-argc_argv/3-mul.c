1ff2ded67ea8ce6041be161c2d6c8ac6b3adf67d
edoka <peter.edoka.242464@unn.edu.ng>
2023-04-06 19:18:32 +0100

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that multiplies two numbers.
+ * @argc: the argument count.
+ * @argv: the array of arguments
+ *
+ * Return: 0 on success, 1 otherwise
+*/
+int main(int argc, char *argv[])
+{
+    int x, y;
+
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return (1);
+    }
+
+    x = atoi(argv[1]);
+    y = atoi(argv[2]);
+
+    printf("%d\n", x * y);
+
+	return (0);
+}
