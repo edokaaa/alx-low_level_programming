11b4e8c62511ffa3d8e0f27dc0914febbd797e9b
edoka <peter.edoka.242464@unn.edu.ng>
2023-04-06 19:01:57 +0100

    argv[0]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of program
+ * @argc: the argument count.
+ * @argv: the array of arguments
+ *
+ * Return: 0
+*/
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int main(int __attribute__ ((unused)) argc, char *argv[]);
+
+
+#endif /* MAIN_H */
