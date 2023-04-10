eaf615292803aba054a3b6b42d9c231760eebdaf
edoka <peter.edoka.242464@unn.edu.ng>
2023-04-10 11:19:02 +0100

    fix: betty errors

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,18 +10,18 @@
 */
 int main(int argc, char *argv[])
 {
-    int x, y;
+	int x, y;
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return (1);
-    }
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    x = atoi(argv[1]);
-    y = atoi(argv[2]);
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
 
-    printf("%d\n", x * y);
+	printf("%d\n", x * y);
 
 	return (0);
 }
