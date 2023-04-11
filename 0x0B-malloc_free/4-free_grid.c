72a947d760d334fba62fa4f5ed2048903bcb0e78
edoka <peter.edoka.242464@unn.edu.ng>
2023-04-11 21:37:52 +0100

    fix: betty coding style

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * free_grid -  frees a 2 dimensional grid previously created by your alloc_grid function.
+ * free_grid -  frees a 2 dimensional grid previously
+ * created by your alloc_grid function.
  * @grid: pointer to the array
  * @height: height of array
  *
