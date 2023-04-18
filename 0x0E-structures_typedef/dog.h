a58664134307ff8c64a69fb9c54fe7697c4b741e
edoka <peter.edoka.242464@unn.edu.ng>
2023-04-18 04:06:22 +0100

    new: creating a structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: This defines a representation of a dog.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
