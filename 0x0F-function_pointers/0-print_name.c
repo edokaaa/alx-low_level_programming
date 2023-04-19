3edcb2568e1916d7b068c1749b4048c16c05e905
edoka <peter.edoka.242464@unn.edu.ng>
2023-04-19 18:06:21 +0100

    new: call function using function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints a name using function pointers.
+ * @name: name to print
+ * @f: pointer to function
+ *
+ * Return: nothing
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
