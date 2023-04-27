ba37361c6725da5122d574ddc69ea0c70f12d7d2
edoka <peter.edoka.242464@unn.edu.ng>
2023-04-27 19:58:10 +0100

    Printing a linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - function that prints all the elements
+ * of a linked lists
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes on the list
+*/
+size_t print_list(const list_t *h)
+{
+	size_t l = 0;
+
+	if (h == NULL)
+	{
+		return (1);
+	}
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		l++;
+		h = h->next;
+	}
+	return (l);
+}
