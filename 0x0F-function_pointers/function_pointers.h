2c5bbc9d0c4d9fb1bc55dc357be6cdc4f44a128a
edoka <peter.edoka.242464@unn.edu.ng>
2023-04-28 12:10:44 +0100

    fix: updated size_t definition to use stdlib.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdlib.h>
 
-typedef unsigned long size_t;
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
