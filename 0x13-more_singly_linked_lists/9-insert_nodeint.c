ba71ecc9e6ec98c62c3d3238985dfcf2df2e0114
edoka <peter.edoka.242464@unn.edu.ng>
2023-05-01 09:55:58 +0100

    fix: disallow insertion at index -1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	i = 0;
 	while (prev != NULL)
 	{
-		if (idx == i || idx == i + 1)
+		if ((idx == i || idx == i + 1) && prev->next != NULL)
 		{
 			new = malloc(sizeof(listint_t));
 			if (new == NULL)
