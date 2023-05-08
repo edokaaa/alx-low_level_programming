71112ceab472dfba39285f30b32e850cf313907e
edoka <peter.edoka.242464@unn.edu.ng>
2023-05-08 08:00:39 +0100

    task: setting a value of a bit to 1 at given index

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,6 @@ int _pow_recursion(int x, int y);
 void print_binary(unsigned long int n);
 void _binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif /* MAIN_H */
