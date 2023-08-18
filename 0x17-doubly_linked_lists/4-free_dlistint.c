bd8e5bb49988465049f622bd90540dd0648acb4f
edoka <peter.edoka.242464@unn.edu.ng>
2023-08-18 05:45:56 +0100

    fixed betty

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -8,12 +8,12 @@
 */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *tmp;
+	dlistint_t *tmp;
 
-    while (head != NULL)
-    {
-        tmp = head->next;
-        free(head);
-        head = tmp;
-    }
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
 }
