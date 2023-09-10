90e8309c9a90092a168000fa9ea73180df89245e
edoka <peter.edoka.242464@unn.edu.ng>
2023-09-10 13:54:10 +0100

    a function implementing the djb2 algorithm

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - a function implementing the djb2 algorithm
+ * This function will be our hashing function.
+ *
+ * @str: this will be our input key
+ *
+ * Return: It will always return a hash value.
+*/
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	
+	return (hash);
+}
