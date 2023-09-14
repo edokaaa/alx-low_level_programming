02226c46644e3a4a88e742c894093f51689efb3e
edoka <peter.edoka.242464@unn.edu.ng>
2023-09-14 05:14:12 +0100

    printing and hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_print - A function that prints an hash table
+ * @ht: hash table
+ *
+ * Return: nothing
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	int first = 1;
+
+	if (ht == NULL)
+		return;
+	
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (!first)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			first = 0;
+			node = node->next;
+		}
+	}
+
+	printf("}\n");
+}
