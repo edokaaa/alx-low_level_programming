c7edb15ad08ca8ad754bcdb751b2695327dd9029
edoka <peter.edoka.242464@unn.edu.ng>
2024-03-15 23:47:01 +0100

    chore: binary search init

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+/**
+ * binary_search - a function that searches for a value in a sorted array
+ * of intergers using the Binary search algorithm.
+ * @array: pointer to the firt element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for.
+ *
+ * Return: index where value is located or -1 if otherwise.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	return (0);
+}
