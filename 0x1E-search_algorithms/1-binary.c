bf407ea700618c1aa1529e2034582f547354fb4c
edoka <peter.edoka.242464@unn.edu.ng>
2024-05-08 00:21:49 +0100

    binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,5 +12,52 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
+	size_t left, right, middle, i = 0;
+
+	left = 0;
+	right = size - 1;
+
+	while (left <= right)
+	{
+		print_array(&(array[left]), right - left + 1);
+
+		middle = (left + right) / 2;
+
+		if (array[middle] < value)
+			left = middle + 1;
+		else if (array[middle] > value)
+			right = middle - 1;
+		else
+			return (middle);
+		i++;
+	}
+	return (-1);
+}
+
+/**
+ * print_array - prints the searched array.
+ * @array: pointer to the first element of the array to print.
+ * @size: number of elements to print
+ *
+ * Return: nothing.
+*/
+int print_array(int *array, size_t size)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	printf("Searching in array: ");
+
+	while (i < size)
+	{
+		printf("%d", array[i]);
+		if (i < size - 1)
+			printf(", ");
+		else
+			printf("\n");
+		i++;
+	}
 	return (0);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,5 +5,6 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int print_array(int *array, size_t size);
 
 #endif /*SEARCH_H*/
